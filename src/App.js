87383181bc4bf7b8420cc60a993359c074d6a6e2
Rudy Ravelin <rudy@noukod.com>
2019-04-02 16:04:39 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
-import CatComponent from './CatComponent.js'
+import CatComponent from './CatComponent';
 import { EinsteinQuoteComponent } from './EinsteinQuoteComponent';
-import MouseComponent from '../MouseComponent'
+import MouseComponent from './MouseComponent';
 
 class App extends Component {
-  render() {
-    // your code in the return statement below!
-    return (
-      <div className="App">
-        <CatComponent />
-        <EinsteinQuoteComponent />
-        <MouseComponent />
-      </div>
-    );
-  }
+	render() {
+		// your code in the return statement below!
+		return (
+			<div className="App">
+			< CatComponent />
+			< EinsteinQuoteComponent />
+			< MouseComponent />
+			</div>
+		);
+	}
 }
 
 export default App;
