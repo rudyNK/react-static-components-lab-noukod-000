e61ac54616428911afce5bd29281a64b49328736
Rudy Ravelin <rudy@noukod.com>
2019-04-02 15:52:10 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
     return (
       <div className="App">
         <CatComponent />
-        <FordQuoteComponent />
+        // <FordQuoteComponent />
         <MouseComponent />
       </div>
     );
